2ac5d186944b1609bc6ab574182b39d4ab2e9f6f
Maxim Yevgrafov <yevgrafov.max@gmail.com>
2022-02-25 14:18:38 +0300

    add skript.sh

---
+++ skript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir NewFolder1
+cd NewFolder1
+pwd
+mkdir folder1 folder2 folder3
+cd folder1
+touch file1.txt file2.txt file3.txt ff.json dd.json
+mkdir work1 work2 work3
+ls -la
+mv file1.txt file2.txt work2
